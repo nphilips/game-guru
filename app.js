522b3147d3834440ca4806573819090060c659ff
nphilips <nicholasphilips622@gmail.com>
2019-05-14 19:13:12 -0600

    git update

---
+++ app.js
@@ -0,0 +1,18 @@
+{/* <img onmouseover="start" src="gears-of-war.png" alt="gears-of-war"></img> */}
+
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myNavBar");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
